ec79aa105453db70dbca6f3a9676561443741af4
anilkumar <anilkumar567849@gmail.com>
2025-01-31 23:14:15 +0530

    logs removed

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -50,14 +50,11 @@ This POST function is used to receive messages from the customer. When a custome
 We can reply back to customer messages by sending a POST request to the Facebook API with the customer phone number and message data. URL: https://graph.facebook.com/v13.0/${phon_no_id}/messages?access_token=${token}
 */
 export async function POST(req: NextRequest) {
-    console.log('POST Request Received');
 
     try {
         const body_param = await req.json();
-        console.log('body_param:', JSON.stringify(body_param, null, 2));
 
         if (!body_param || !body_param.object) {
-            console.log("Invalid request body");
             return new NextResponse("Invalid request body", { status: 400 });
         }
 
@@ -71,16 +68,12 @@ export async function POST(req: NextRequest) {
             const from = messages.from;
             const msg_body = messages.text?.body || "";
 
-            console.log('msg received from:', from, 'msg:', msg_body, 'phone:', phon_no_id);
-
             // Ensure helper function calls are awaited correctly
             // await WhatsAppHelper.saveCustomerData(phon_no_id, from, msg_body);
             await WhatsAppHelper.sendMsgToUser(phon_no_id, from, msg_body);
 
             console.log('msg sent to:', from);
             return new NextResponse(null, { status: 200 });
-        } else {
-            console.log("No valid message data found.");
         }
     } catch (error) {
         console.error("Error processing request:", error);
